3b017944c0f7f3717d0841733ca39465e585bf14
cglu <shouhuni.xue@qq.com>
2016-03-04 17:54:48 +0800

    增加index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$str= <<<'EOD'
+<script>
+     document.domain = 'lubu.in'
+// 计算页面的实际高度，iframe自适应会用到
+function calcPageHeight(doc) {
+var cHeight = Math.max(doc.body.clientHeight, doc.documentElement.clientHeight)
+var sHeight = Math.max(doc.body.scrollHeight, doc.documentElement.scrollHeight)
+var height  = Math.max(cHeight, sHeight)
+return height
+}
+window.onload = function() {
+var height = calcPageHeight(document)
+parent.document.getElementsByTagName('iframe')[0].style.height = height + 'px'     
+}
+</script></body>
+EOD;
+if (!isset($_GET['id'])) {
+    echo "没有发现id参数";
+    exit(0);
+}
+$file_path=$_GET['id'].".html";
+if (!file_exists($file_path)) {
+    echo "当前目录下没有发现{$file_path}文件.请联系管理员.";
+    exit(0);
+}
+$content=file_get_contents($file_path);
+$content=str_replace('</body>', $str, $content);
+echo $content;
